95c228b75b0844e21bd689dd4c92cf242d281a17
Hector Rodriguez Fornies <torhector2@gmail.com>
2020-01-24 16:41:28 +0700

    feat: Answered question page shows votes and percentage

--- would-you-rather/src/components/AnsweredQuestionPage.js
+++ would-you-rather/src/components/AnsweredQuestionPage.js
@@ -8,10 +8,19 @@ class AnsweredQuestionPage extends Component {
         const optionTwoVotes = question.optionTwo.votes.length
         const totalVotes = optionOneVotes + optionTwoVotes
         return(
-            <div>Results:
-                <ResultCard optionText={question.optionOne.text} votes={optionOneVotes} totalVotes={totalVotes} voted={optionVoted === 'optionOne'}/>
-                <ResultCard optionText={question.optionTwo.text} votes={optionTwoVotes} totalVotes={totalVotes} voted={optionVoted === 'optionTwo'}/>
+            <div>
+                <div className="ml-8 text-gray-700 text-xl">Asked by {author.name}</div>
+                <div className="w-full max-w-full flex mt-8 mb-8 text-gray-900">
+                    <div className="ml-4 h-56 w-56 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${author.avatarURL})`}}>
+                    </div>
+                    <div>
+                        <p className="ml-4 text-gray-700 text-4xl">Results:</p>
+                        <ResultCard optionText={question.optionOne.text} votes={optionOneVotes} totalVotes={totalVotes} voted={optionVoted === 'optionOne'}/>
+                        <ResultCard optionText={question.optionTwo.text} votes={optionTwoVotes} totalVotes={totalVotes} voted={optionVoted === 'optionTwo'}/>
+                    </div>
+                </div>
             </div>
+            
         )
     }
 }
--- would-you-rather/src/components/ResultCard.js
+++ would-you-rather/src/components/ResultCard.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react'
 class ResultCard extends Component {
     render() {
         const { optionText, votes, totalVotes, voted } = this.props
-        console.log(votes)
-        const percentage = totalVotes !== 0 ? votes/totalVotes * 100 : 0
+        const percentage = (totalVotes !== 0 ? votes/totalVotes * 100 : 0).toFixed(1)
         return (
-            <div>
-                <p>{optionText}</p>
-                <p>{votes}</p>
-                <p>{totalVotes}</p>
-                <p>{percentage.toFixed(1)}</p>
-                <p>Voted: {voted ? "Voted" : "Not voted"}</p>
+            <div className={`mt-4 ml-4 mr-4 w-11/12 border border-gray-600 p-4 rounded ${voted ? 'bg-teal-100' : ''}`}>
+                { voted && 
+                    (<div class="flex">
+                        <div className="mr-2 bg-teal-600 text-white p-2 rounded leading-none flex items-center">
+                            Your Vote
+                        </div>
+                    </div>)  
+                }
+                
+                <div className="text-teal-900 font-bold text-lg">Would you rather {optionText}</div>
+                <div className="shadow w-full bg-gray-300 mt-2">
+                    <div className="bg-teal-400 text-xs leading-none py-1 text-center text-white font-bold" style={{width: `${percentage}%`}} >{percentage}%</div>
+                </div>
+                <div className="text-gray-800 font-bold text-center">{votes} out of {totalVotes} votes</div>
             </div>
         )
     }
