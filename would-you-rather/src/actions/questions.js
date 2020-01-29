2f402f9907a3b66672d6644d191f51a155aadc29
Hector Rodriguez Fornies <torhector2@gmail.com>
2020-01-29 11:13:22 +0700

    refactor: remove unnecessary action from questions

--- would-you-rather/src/actions/questions.js
+++ would-you-rather/src/actions/questions.js
@@ -1,15 +1,7 @@
 import { _saveQuestionAnswer, _saveQuestion } from '../utils/_DATA'
 import { handleInitialData } from './shared'
 
-export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 export const ANSWER_QUESTION = 'ANSWER_QUESTION'
-
-export function receiveQuestions(questions) {
-    return {
-        type: RECEIVE_QUESTIONS,
-        questions
-    }
-}
 
 function answerQuestion(authedUser, qid, answer) {
     return {
